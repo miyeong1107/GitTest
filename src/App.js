e665b365149070364d577d397dad14cd13d521d8
Miyeong Choi <miyeongchoi@Miyeongui-MacBookPro.local>
2021-09-15 21:07:46 +0900

    GetTestt3333

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="AppGitTest">
+    <div className="AppGitTest@@@@@">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
